a12d4354b43e18636a095b0666dc31bb3a078086
root <root@master.localdomain>
2016-03-21 08:41:45 -0400

    * Added logic for remote servers SM

--- create.sh
+++ create.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-mkdir -p /srv/data/pv0001 /srv/data/pv0002
-chcon -Rt svirt_sandbox_file_t /srv/data/pv000*
+
+dir="/srv/data"
+
+for i in `oc get nodes | awk '{print $1}' | grep -v ^NAME$`
+do
+	for j in pv0001 pv0002 pv0003 pv0004
+	do
+		ssh root@$i mkdir -p $dir/$j
+		ssh root@$i chmod 777 $dir/$j
+		ssh root@$i chcon -Rt svirt_sandbox_file_t $dir/$j
+	done
+done
 oc new-project wordpress
 oc create -f wordpress-objects.yaml
