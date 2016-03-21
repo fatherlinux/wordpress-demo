741bebd493895a4df465b2d011dbb8ec6ab18144
root <root@master.localdomain>
2016-03-21 09:02:06 -0400

    * fixed directory variable * Add quiet options * Added debug output SM

--- delete.sh
+++ delete.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+dir=/srv/data
+
 oc delete pv pv0001 pv0002 pv0003 pv0004
 oc delete project wordpress
 for i in `oc get nodes | awk '{print $1}' | grep -v ^NAME$`
 do
-	ssh root@$i rm -rf $dir/pv000*
+	ssh -q root@$i rm -rf $dir/pv000*
+	echo "Host: $i"; ssh -q root@$i "ls -alhZ $dir"
 done
 
