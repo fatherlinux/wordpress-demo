5c8edb4d1da14d8193690bc8bab17c6fb119fbc0
root <root@master.localdomain>
2016-03-21 09:02:40 -0400

    * Changed to quiet because of weirdness with Ravello and keys * Added debug output SM

--- create.sh
+++ create.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+
 dir="/srv/data"
 
 for i in `oc get nodes | awk '{print $1}' | grep -v ^NAME$`
 do
 	for j in pv0001 pv0002 pv0003 pv0004
 	do
-		ssh root@$i mkdir -p $dir/$j
-		ssh root@$i chmod 777 $dir/$j
-		ssh root@$i chcon -Rt svirt_sandbox_file_t $dir/$j
+		ssh -q root@$i mkdir -p $dir/$j
+		ssh -q root@$i chmod 777 $dir/$j
+		ssh -q root@$i chcon -Rt svirt_sandbox_file_t $dir/$j
+		ssh -q root@$i ls -alhZ $dir/$j
 	done
 done
 oc new-project wordpress
