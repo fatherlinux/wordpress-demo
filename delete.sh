8f1ebe9dda5109c7d7d440e015ea7138cbe3db7c
root <root@master.localdomain>
2016-03-24 07:12:32 -0400

    * Cleaned up scripts to run a bit faster and more efficiently SM

--- delete.sh
+++ delete.sh
@@ -4,11 +4,11 @@ dir=/srv/data
 
 oc delete pv pv0001 pv0002 pv0003 pv0004
 oc delete project wordpress
-for i in `oc get nodes | awk '{print $1}' | grep -v ^NAME$`
+for i in `oc get nodes | awk '{print $1}' | grep node0`
 do
 	echo "Host: $i"
-	ssh -q root@$i rm -rf $dir/pv000*
-	ssh -q root@$i "ls -alhZ $dir"
-	ssh -q root@$i "docker rm `docker ps --no-trunc -a -q`"
-	ssh -q root@$i "docker rmi docker.io/wordpress"
+	ssh -q root@$i "rm -rf $dir/pv000* &&\
+	                ls -alhZ $dir &&\
+	                docker rm `docker ps --no-trunc -a -q` &&\
+	                docker rmi docker.io/wordpress"
 done
