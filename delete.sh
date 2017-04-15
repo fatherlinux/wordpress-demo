16a3adf2945542b384b7b1f98be721a7107da7e5
root <root@master1.ocp1.dc2.crunchtools.com>
2017-04-15 02:26:05 +0000

    SM: Updated delete script

--- delete.sh
+++ delete.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-dir=/srv/data
-
-oc delete pv pv0001 pv0002 pv0003 pv0004
-oc delete project wordpress
-for i in `oc get nodes | awk '{print $1}' | grep node0`
-do
-	echo "Host: $i"
-	ssh -q root@$i "rm -rf $dir/pv000* &&\
-	                ls -alhZ $dir &&\
-	                docker rm \`docker ps --no-trunc -a -q\` &&\
-	                docker rmi docker.io/wordpress"
-done
+oc delete rc/wordpress rc/mysql pvc/claim-wp pvc/claim-mysql pv/pv0001 pv/pv0002 pv/pv0003 pv/pv0004
