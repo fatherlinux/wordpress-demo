70a17c5d4109f5e238813935c08a1b7e9c812844
root <root@master1.ocp1.dc2.crunchtools.com>
2017-04-15 16:26:00 +0000

    SM: Separated application and storage objects for easy control when experimenting/testing/teaching

--- delete.sh
+++ delete.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-oc delete rc/wordpress rc/mysql pvc/claim-wp pvc/claim-mysql pv/pv0001 pv/pv0002 pv/pv0003 pv/pv0004
+# Deletes the application objects
+oc delete rc/wordpress rc/mysql pvc/claim-wp pvc/claim-mysql
+
+# Deletes the storage objects. Can be commented out to maintain data between instantiations.
+oc delete pv/pv0001 pv/pv0002 pv/pv0003 pv/pv0004
