bd4f0f6be9cd98442c179fa14a609d18d96fedd7
Scott McCarty <scott.mccarty@gmail.com>
2017-10-07 13:46:48 -0400

    Fixed script

--- create-pv.sh
+++ create-pv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-dir=/exports/data/
+dir=/srv/data
 
 for i in pv0001 pv0002 pv0003 pv0004
 do
-	mkdir -p "$dir/$i
-	chmod 777 $dir/$i
-	chcon -Rt svirt_sandbox_file_t $dir/$i"
+        mkdir -p $dir/$i
+        chmod 777 $dir/$i
+        chcon -Rt svirt_sandbox_file_t $dir/$i
 done
