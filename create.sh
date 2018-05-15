ca43704ee6f6f7ec8bb2c6f83a26d8be33acf75b
Scott McCarty <smccarty@redhat.com>
2018-05-15 14:39:26 -0400

    SM: changed create script for labs

--- create.sh
+++ create.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
-if [ "$1" == "" ]
-then
-	echo "Please specifiy a yaml or json file. Example: ./create.sh wordpress-objects.yaml"
-	exit
-fi
+dir=/srv/data
 
-dir="/srv/data"
-
-for i in `oc get nodes | awk '{print $1}' | grep node0`
+for i in pv0001 pv0002 pv0003 pv0004
 do
-	echo "Node: $i"
-	for j in pv0001 pv0002 pv0003 pv0004
-	do
-		ssh -q root@$i mkdir -p "$dir/$j && \
-		               chmod 777 $dir/$j && \
-		               chcon -Rt svirt_sandbox_file_t $dir/$j"
-	done
-	ssh -q root@$i "ls -alhZ $dir/"
+        mkdir -p $dir/$i
+        chmod 777 $dir/$i
+        chcon -Rt svirt_sandbox_file_t $dir/$i
 done
-
-# First create the persistent volumes:
-oc create -f persistent-volumes.yaml
-oc new-project wordpress
-oc create -f $1
